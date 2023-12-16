2c70f7c8c75af0a0a60c081c6c7e9ead9e6b6053
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2023-12-16 12:56:08 -0500

    Update AetherArmorMaterials.java

--- src/main/java/com/aetherteam/aether/item/combat/AetherArmorMaterials.java
+++ src/main/java/com/aetherteam/aether/item/combat/AetherArmorMaterials.java
@@ -15,9 +15,9 @@ public enum AetherArmorMaterials implements ArmorMaterial {
 	ZANITE("zanite", 15, new int[]{2, 5, 6, 2}, 9, AetherSoundEvents.ITEM_ARMOR_EQUIP_ZANITE, 0.0F, () -> Ingredient.of(AetherTags.Items.ZANITE_REPAIRING)),
 	GRAVITITE("gravitite", 33, new int[]{3, 6, 8, 3}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_GRAVITITE, 2.0F, () -> Ingredient.of(AetherTags.Items.GRAVITITE_REPAIRING)),
 	NEPTUNE("neptune", 15, new int[]{2, 5, 6, 2}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_NEPTUNE, 1.0F, () -> Ingredient.of(AetherTags.Items.NEPTUNE_REPAIRING)),
-	VALKYRIE("valkyrie", 33, new int[]{3, 6, 8, 3}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_VALKYRIE, 2.0F, () -> Ingredient.of(AetherTags.Items.VALKYRIE_REPAIRING)),
-	PHOENIX("phoenix", 33, new int[]{3, 6, 8, 3}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_PHOENIX, 2.0F, () -> Ingredient.of(AetherTags.Items.PHOENIX_REPAIRING)),
-	OBSIDIAN("obsidian", 37, new int[]{3, 6, 8, 3}, 15, AetherSoundEvents.ITEM_ARMOR_EQUIP_OBSIDIAN, 3.0F, () -> Ingredient.of(AetherTags.Items.OBSIDIAN_REPAIRING)),
+	VALKYRIE("valkyrie", 33, new int[]{3, 6, 8, 3}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_VALKYRIE, 3.0F, () -> Ingredient.of(AetherTags.Items.VALKYRIE_REPAIRING)),
+	PHOENIX("phoenix", 33, new int[]{3, 6, 8, 3}, 10, AetherSoundEvents.ITEM_ARMOR_EQUIP_PHOENIX, 3.0F, () -> Ingredient.of(AetherTags.Items.PHOENIX_REPAIRING)),
+	OBSIDIAN("obsidian", 37, new int[]{3, 6, 8, 3}, 15, AetherSoundEvents.ITEM_ARMOR_EQUIP_OBSIDIAN, 4.0F, () -> Ingredient.of(AetherTags.Items.OBSIDIAN_REPAIRING)),
 	SENTRY("sentry", 15,  new int[]{2, 5, 6, 2}, 9, AetherSoundEvents.ITEM_ARMOR_EQUIP_SENTRY, 0.0F, () -> Ingredient.of(AetherTags.Items.SENTRY_REPAIRING));
 
 	private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
